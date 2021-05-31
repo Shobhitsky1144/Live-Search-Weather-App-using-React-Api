dd1eb03125be526d2bd0a3326a967820f1acb6f2
Shobhit Yadav <72183704+Shobhitsky1144@users.noreply.github.com>
2021-05-31 10:06:30 +0530

    Update Tempapp.jsx

--- src/Components/Tempapp.jsx
+++ src/Components/Tempapp.jsx
@@ -10,11 +10,7 @@ const Tempapp = () => {
     useEffect(() => {
         const fetchApi = async () => {
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=83f89d90353a7401ad33bc5cf4892337`
-            const response = await fetch(url, {
-      mode: 'no-cors'
- })
-                  
-//                   await fetch(url);
+            const response =await fetch(url);
 
             const resJson = await response.json();
             //   console.log(resJson);
